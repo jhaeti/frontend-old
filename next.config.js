11ceb7db8523842ee52b7ba29493c3cd4f67f3e1
Ti Jhae <jhaeti@gmail.com>
2022-07-19 16:52:47 +0000

    Enabled hot reloading with env and small tweak in nex config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-}
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+    },
+    webpackDevMiddleware: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
