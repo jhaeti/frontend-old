da3aca21386a8d6d413706384c220c3181269dd1
Ti Jhae <jhaeti@gmail.com>
2022-07-19 11:28:37 +0000

    Added formatting and linting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,21 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
+import type { AppProps } from "next/app";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import GlobalStyle from "../components/globalstyles";
 
 const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
-}
+    colors: {
+        primary: "#111",
+        secondary: "#0070f3",
+    },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,22 @@
-import Link from 'next/link'
-import { Container, Main, Title, Description } from '../components/sharedstyles'
+import Link from "next/link";
+import {
+    Container,
+    Main,
+    Title,
+    Description,
+} from "../components/sharedstyles";
 
 export default function About() {
-  return (
-    <Container>
-      <Main>
-        <Title>About Page</Title>
-        <Description>
-          <Link href="/">
-            <a>&larr; Go Back</a>
-          </Link>
-        </Description>
-      </Main>
-    </Container>
-  )
+    return (
+        <Container>
+            <Main>
+                <Title>About Page</Title>
+                <Description>
+                    <Link href="/">
+                        <a>&larr; Go Back to where ever you came from</a>
+                    </Link>
+                </Description>
+            </Main>
+        </Container>
+    );
 }
