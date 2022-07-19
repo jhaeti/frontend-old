d85fb8b7c93aa5bc33d5f52b9625a6e92d252b81
Ti Jhae <jhaeti@gmail.com>
2022-07-19 15:43:41 +0000

    Just format changed

--- src/components/globalstyles.tsx
+++ src/components/globalstyles.tsx
@@ -1,23 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    color: ${({ theme }) => theme.colors.primary};
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
+	html,
+	body {
+		color: ${({ theme }) => theme.colors.primary};
+    	padding: 0;
+    	margin: 0;
+    	font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  	}
 
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+  	a {
+    	color: inherit;
+    	text-decoration: none;
+  	}
 
   * {
-    box-sizing: border-box;
-  }
+    	box-sizing: border-box;
+    }
 `;
 
 export default GlobalStyle;
