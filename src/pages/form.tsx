7519247eee83a7baa9b12bcd905d63d2f8ebe753
Ti Jhae <jhaeti@gmail.com>
2022-07-25 10:07:54 +0000

    PAge for form

---
+++ src/pages/form.tsx
@@ -0,0 +1,14 @@
+import FormTest from "../components/Form";
+import { Container } from "../components/sharedstyles";
+
+const form = () => {
+    return (
+        <div>
+            <Container>
+                <FormTest />
+            </Container>
+        </div>
+    );
+};
+
+export default form;
