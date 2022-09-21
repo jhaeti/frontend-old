e277300352ec5130268b131f358b799f1b511465
Ti Jhae <jhaeti@gmail.com>
2022-09-21 08:58:43 +0000

    Fixed store auto complete not working when using state with useApp dispatch

--- src/redux/store/hooks.ts
+++ src/redux/store/hooks.ts
@@ -1,6 +1,9 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { RootState, AppDispatch } from ".";
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import type { RootState, AppDispatch } from ".";
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+// export const useAppDispatch = () => useDispatch<AppDispatch>();
 
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
