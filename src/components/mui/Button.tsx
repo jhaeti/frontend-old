2cbe6c615cc19b5c25bcef7d6370b43de751a5c9
Ti Jhae <jhaeti@gmail.com>
2022-09-27 18:53:58 +0000

    Learning mui toggle button component

--- src/components/mui/Button.tsx
+++ src/components/mui/Button.tsx
@@ -1,5 +1,25 @@
-import { Button, Stack } from "@mui/material";
+import { useState } from "react";
+import {
+    Button,
+    Stack,
+    IconButton,
+    ButtonGroup,
+    ToggleButtonGroup,
+    ToggleButton,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
 const MyButton = () => {
+    const [formats, setFormats] = useState<string | null>(null);
+    const handleFormatChange = (
+        e: React.MouseEvent<HTMLElement>,
+        updatedFormats: string | null
+    ) => {
+        setFormats(updatedFormats);
+    };
+
     return (
         <Stack spacing={4}>
             <Stack spacing={2} direction="row">
@@ -28,9 +48,69 @@ const MyButton = () => {
                 </Button>
             </Stack>
             <Stack spacing={2} direction="row" display="block">
-                <Button variant="contained" size="small">Small</Button>
-                <Button variant="contained" size="medium">Medium</Button>
-                <Button variant="contained" size="large">Larg</Button>
+                <Button variant="contained" size="small">
+                    Small
+                </Button>
+                <Button variant="contained" size="medium">
+                    Medium
+                </Button>
+                <Button variant="contained" size="large">
+                    Larg
+                </Button>
+            </Stack>
+            <Stack spacing={2} direction="row">
+                <Button
+                    variant="contained"
+                    startIcon={<SendIcon />}
+                    disableRipple
+                    onClick={() => console.log("Clicked")}
+                >
+                    Send
+                </Button>
+                <Button
+                    variant="contained"
+                    endIcon={<SendIcon />}
+                    disableElevation
+                >
+                    Send
+                </Button>
+                <IconButton aria-label="send" color="success" size="small">
+                    <SendIcon />
+                </IconButton>
+            </Stack>
+            <Stack direction="row">
+                <ButtonGroup
+                    variant="contained"
+                    aria-label="alignment button group"
+                    size="small"
+                    color="secondary"
+                    orientation="vertical"
+                >
+                    <Button onClick={() => alert("Left")}>Left</Button>
+                    <Button onClick={() => alert("Center")}>Center</Button>
+                    <Button onClick={() => alert("Right")}>Right</Button>
+                </ButtonGroup>
+            </Stack>
+            <Stack direction="row">
+                <ToggleButtonGroup
+                    color="success"
+                    exclusive
+                    value={formats}
+                    size="large"
+                    orientation="horizontal"
+                    aria-label="text formatting"
+                    onChange={handleFormatChange}
+                >
+                    <ToggleButton value="bold" aria-label="bold">
+                        <FormatBoldIcon />
+                    </ToggleButton>
+                    <ToggleButton value="italic" aria-label="italic">
+                        <FormatItalicIcon />
+                    </ToggleButton>
+                    <ToggleButton value="underlined" aria-label="underlined">
+                        <FormatUnderlinedIcon />
+                    </ToggleButton>
+                </ToggleButtonGroup>
             </Stack>
         </Stack>
     );
